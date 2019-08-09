224659bb4b187008ad28ab6500262b5f1dd8f314
TungPhi <46023194+TungPhi@users.noreply.github.com>
2019-08-09 20:27:24 +0700

    Add files via upload

---
+++ btapvn.js
@@ -0,0 +1,49 @@
+// var thang = prompt("Vui lòng nhập tháng vào đây")
+// if (thang ==11 || thang ==12 ||thang ==1 ) {
+//     console.log("Đây là mùa Đông");
+// } else if (thang<5) {
+//     console.log("Đây là mùa Xuân");
+// } else if (thang<8) {
+//     console.log("Đây là mùa Hạ");
+// }else{
+//     console.log("Đây là mùa Thu");
+// }
+//-----------------------------Bai 2--------------------------------
+// document.getElementById("demo").innerHTML =
+// Math.floor(Math.random() * 101);
+//-----------------------------Bai 3--------------------------------
+// var a = document.getElementById("demo").innerHTML =Math.random() * 100;
+// if ( a<60) {
+//     console.log("Coudy");
+    
+// }else if (a<30) {
+//     console.log("Ranny");
+// }else{
+//     console.log("Sunny");
+// }
+
+//-----------------------------Bai 4--------------------------------
+// var a = Math.floor(1234/10);
+
+// console.log(a);
+//-----------------------------Bai 5--------------------------------
+    
+// const a = prompt ("Enter a number");
+// var n = a.length;
+// console.log ("The number you entered has",n, "digits");
+//-----------------------------Bai 6--------------------------------
+// var a = Number(prompt("Vui lòng nhập a vào đây"))
+// var b = Number(prompt("Vui lòng nhập b vào đây"))
+// var c = Number(prompt("Vui lòng nhập c vào đây"))
+// var delta = b*b-4*a*c;
+// if (delta>0) {
+//     console.log("Phương Trình có 2 nghiệm phân biệt x1 ="+(-b+Math.sqrt(delta))/(2*a) + "x2 ="+(-b-Math.sqrt(delta))/(2*a) );
+
+// }else if (delta == 0) {
+//     console.log("Phương Trình có nghiệm kép x="+ (-b/(2*a)));
+// }
+// else{
+//     console.log("Phương trình vô nghiệm");
+    
+// }
+//-----------------------------Bai 7--------------------------------
